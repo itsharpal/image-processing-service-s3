c65cb7536368245d0f880ed217ab6bea8c6670a7
HARPAL <harpal.chapatwala.official@gmail.com>
2025-11-10 19:13:36 +0530

    minor updates

--- configs/redis.js
+++ configs/redis.js
@@ -2,7 +2,7 @@ import { createClient } from 'redis';
 
 const redisClient = createClient({
     username: 'default',
-    password: 'YYpvPjWB8rmAYVfn5bCimEgBnbiVVkqP',
+    password: `${process.env.REDIS_PASSWORD}`,
     socket: {
         host: `${process.env.REDIS_HOST}`,
         port: process.env.REDIS_PORT,
