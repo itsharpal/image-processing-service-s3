80811947867f279cb71aad7cc6b68e3078129e95
HARPAL <harpal.chapatwala.official@gmail.com>
2025-11-10 13:19:43 +0530

    image upload controller done

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -1,3 +1,33 @@
 import { uploadToS3, getFromS3, deleteFromS3 } from "../utils/s3.js";
+import sharp from 'sharp'
 import { Image } from '../models/image.model.js';
 
+export const uploadImage = async (req, res) => {
+    try {
+        const { file } = req;
+        const userId = req.userId;
+
+        if (!file || !userId) {
+            return res.status(400).json({ message: "File and userId are required" });
+        }
+
+        const imageUrl = await uploadToS3(file);
+        const image = await Image.create({
+            url: imageUrl,
+            userId
+        })
+
+        res.status(201).json({
+            success: true,
+            message: "Image uploaded successfully",
+            imageUrl,
+        });
+    } catch (error) {
+        console.error("❌ Upload Error:", error);
+        res.status(500).json({
+            success: false,
+            message: "Error uploading image",
+            error: error.message,
+        });
+    }
+};
